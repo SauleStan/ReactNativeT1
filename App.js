bd78cc60928bb1893d32c7dc4652ce64e311f386
SauleStan <vogellietuva@gmail.com>
2020-09-20 18:00:52 +0300

    added color variables

--- App.js
+++ App.js
@@ -6,27 +6,34 @@ import BoxComponent from './components/boxComponent';
 
 
 export default function App() {
-    const [boxColor1, setBoxColor1] = useState('crimson');
-    const [boxColor2, setBoxColor2] = useState('crimson');
-    const [boxColor3, setBoxColor3] = useState('crimson');
-    const [boxColor4, setBoxColor4] = useState('crimson');
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <AppButton onPress={() => {setBoxColor1('green')}} title="Button1"/>
-          <AppButton onPress={() => {setBoxColor2('green')}} title="Button2"/>
-          <AppButton onPress={() => {setBoxColor3('green')}} title="Button3"/>
-          <AppButton onPress={() => {setBoxColor4('green')}} title="Button4"/>
-        </View>
-        <View style={styles.boxContainer}>
-          <BoxComponent color={boxColor1} text={"BOX1"}></BoxComponent>
-          <BoxComponent color={boxColor2} text={"BOX2"}></BoxComponent>
-          <BoxComponent color={boxColor3} text={"BOX3"}></BoxComponent>
-          <BoxComponent color={boxColor4} text={"BOX4"}></BoxComponent>
-        </View>
+  const initialColor = 'crimson';
+  const changedColor = 'green';
+
+  const [boxColor1, setBoxColor1] = useState(initialColor);
+  const [boxColor2, setBoxColor2] = useState(initialColor);
+  const [boxColor3, setBoxColor3] = useState(initialColor);
+  const [boxColor4, setBoxColor4] = useState(initialColor);
+
+  // const prevColorRef = useRef();
+  // prevColorRef.current = 'crimson';
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <AppButton onPress={() => {setBoxColor1(changedColor)}} title="Button1"/>
+        <AppButton onPress={() => {setBoxColor2(changedColor)}} title="Button2"/>
+        <AppButton onPress={() => {setBoxColor3(changedColor)}} title="Button3"/>
+        <AppButton onPress={() => {setBoxColor4(changedColor)}} title="Button4"/>
       </View>
-    );
+      <View style={styles.boxContainer}>
+        <BoxComponent color={boxColor1} text={"BOX1"}></BoxComponent>
+        <BoxComponent color={boxColor2} text={"BOX2"}></BoxComponent>
+        <BoxComponent color={boxColor3} text={"BOX3"}></BoxComponent>
+        <BoxComponent color={boxColor4} text={"BOX4"}></BoxComponent>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
